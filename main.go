1fc99f7520079d9c715ea865868a415f022dc000
Sebastien Binet <binet@cern.ch>
2015-11-23 09:35:37 +0100

    main: add sub-commands skeleton

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 // jupyter is a web-server for Jupyter kernels
 package main
 
+import (
+	"github.com/gonuts/commander"
+	"github.com/gonuts/flag"
+)
+
+var (
+	app *commander.Command
+)
+
+func init() {
+	app = &commander.Command{
+		UsageLine:   "jupyter",
+		Subcommands: nil,
+		Flag:        *flag.NewFlagSet("jupyter", flag.ExitOnError),
+	}
+}
+
 func main() {
+
 }
